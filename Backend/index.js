48e7201f4dee3295e650f2d771808e6c46d0abe2
Devi Intan <deviintannp@gmail.com>
2025-04-20 20:11:16 +0700

    Update isi tugas 5 docker

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import UserRoute from "./routes/noteRoute.js";
 
 const app = express();
+const port = process.env.PORT | 5000;
 
 app.use(cors());
 app.use(express.json());
